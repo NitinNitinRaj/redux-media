1cf6d255285472e2254b9f8a07cb50b522d4d6ee
NitinNitinRaj <bnitinraj@gmail.com>
2023-08-29 19:17:24 +0530

    redux toolkit query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,3 +1,9 @@
+import { useFetchAlbumsQuery } from "../store";
+
 export default function AlbumList({ user }) {
+  const results = useFetchAlbumsQuery(user);
+
+  // console.log(data, error, isLoading);
+  console.log(results);
   return <div>Albums for {user.name}</div>;
 }
